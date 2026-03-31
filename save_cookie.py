a9c561f079b5838dcaeb89692aeba7c247bc5ffe
Kimchiman <qlsdlfghks123@gmail.com>
2026-03-31 22:57:10 +0900

    로그인 쿠키 저장 로직

--- save_cookie.py
+++ save_cookie.py
@@ -6,10 +6,10 @@ from cryptography.fernet import Fernet
 driver = webdriver.Chrome()
 driver.get("https://www.naver.com/")
 
-print("⏳ 1분 안에 네이버 로그인을 직접 완료해 주세요!")
+print("1분 안에 네이버 로그인을 직접 완료해 주세요.")
 time.sleep(60)
 
-# 1. 🔑 나만의 절대 열쇠(Key) 생성
+# 1. Key 생성
 key = Fernet.generate_key()
 cipher_suite = Fernet(key)
 
@@ -17,15 +17,16 @@ cipher_suite = Fernet(key)
 cookies = driver.get_cookies()
 raw_cookie_data = pickle.dumps(cookies)
 
-# 3. 🔒 열쇠를 이용해 쿠키 데이터를 암호화 (잠그기!)
+# 3. 열쇠를 이용해 쿠키 데이터를 암호화
 encrypted_cookie_data = cipher_suite.encrypt(raw_cookie_data)
 
 # 4. 암호화된 데이터를 파일로 저장
 with open("naver_cookies.enc", "wb") as f:
     f.write(encrypted_cookie_data)
 
-print("✅ 쿠키 암호화 저장 완료! (해커가 훔쳐가도 절대 못 풉니다)")
-print("\n🚨 [매우 중요] 아래 열쇠를 복사해서 .env 파일에 넣으세요 🚨")
+
+#쿠키를 생성하고, 터미널 창에 키가 뜨면, 열쇠를 복사해서 .env 파일에 넣으세요 
+
 print(f"COOKIE_KEY={key.decode()}")
 
 driver.quit()
